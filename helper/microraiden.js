6ac1dd76ec7cfb763bc3b40845381d71f41d8305
Mamun Hasan <mamunhasan@w3engineers.com>
2019-03-21 16:30:36 +0600

    web3 reference

--- helper/microraiden.js
+++ helper/microraiden.js
@@ -4,20 +4,15 @@ const EthereumTx = require('ethereumjs-tx');
 const fs = require('fs-extra');
 
 const web3 = new Web3("http://52.8.233.242:8545");
+const contractABI = JSON.parse(fs.readFileSync(__dirname + '/../contracts/contracts.json').toString());
 
-var contractABI = [],
-    tokenAddr = "0x74434527b8e6c8296506d61d0faf3d18c9e4649a",
-    channelAddr = "0xff24d15afb9eb080c089053be99881dd18aa1090";
-
-
-fs.readFile(__dirname + '/../contracts/build/Token.abi', function (err, content) {
-    if (err) {
-        console.log(err);
-        return;
-    }
-    contractABI = JSON.parse(content.toString());
-});
-
+const TOKEN = 'CustomToken';
+const CHANNEL_MANAGER = 'RaidenMicroTransferChannels';
+const TOKEN_SUPLY = 1e25;
+const DECIMALS = 18;
+const CHALLENGE_PERIOD = 500;
+const tokenAddr = "0x74434527b8e6c8296506d61d0faf3d18c9e4649a";
+const channelAddr = "0xff24d15afb9eb080c089053be99881dd18aa1090";
 
 var config = {
     gas: null,
@@ -32,18 +27,30 @@ var MicroRaiden = {
     channel: null,
     token: null,
     getContracts: function () {
-        this.channel = web3.eth.Contract(contractABI["RaidenMicroTransferChannels"], channelAddr);
-        this.token = web3.eth.Contract(contractABI["Token"], tokenAddr);
+        this.channel = web3.eth.Contract(contractABI[CHANNEL_MANAGER].abi, channelAddr);
+        this.token = web3.eth.Contract(contractABI[TOKEN].abi, tokenAddr);
+    },
+    getBalance: function () {
+        web3.eth.getAccounts().then(function (accounts) {
+            var sender_address = '0x18c8bA8eA6Ba89AA3e4a329CF752E71cBA061025';
+            MicroRaiden.token.methods.balanceOf(sender_address).call((error, result) => {
+                console.log(error);
+            }).then((result) => {
+                console.log(result);
+            });
+        });
     },
     getChannelInfo: function () {
-        console.log(MicroRaiden.token.address);
-        console.log(MicroRaiden.channel.address);
-        /*web3.eth.getAccounts().then(function(accounts){
-            var sender_address = '0x18c8bA8eA6Ba89AA3e4a329CF752E71cBA061025', open_block_number = 5227362;
-            MicroRaiden.contract.methods.getChannelInfo(sender_address, accounts[0], open_block_number).call({from: accounts[0]}, (error, result) => {
-
+        web3.eth.getAccounts().then(function (accounts) {
+            var sender_address = EthSigUtil.normalize('0x18c8bA8eA6Ba89AA3e4a329CF752E71cBA061025'),
+                receiver_address = EthSigUtil.normalize(accounts[0]),
+                open_block_number = 5227362;
+            MicroRaiden.channel.methods.getKey(sender_address, receiver_address, open_block_number).call({from: receiver_address}, (error, result) => {
+                console.log(error);
+            }).then((result) => {
+                console.log(result);
             });
-        });*/
+        });
     },
     closeChannel: function (privKey, address, account, count, callback) {
         var transfer = this.channel.closeCoperatively(address);
