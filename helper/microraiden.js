908d6ec3293e9b4f904f23f4fcd5e89914009248
Mamun Hasan <mamunhasan@w3engineers.com>
2019-03-21 18:05:23 +0600

    web3 reference

--- helper/microraiden.js
+++ helper/microraiden.js
@@ -34,7 +34,7 @@ var MicroRaiden = {
     getBalance: function () {
         var sender = EthSigUtil.normalize('0x18c8bA8eA6Ba89AA3e4a329CF752E71cBA061025'),
             receiver = EthSigUtil.normalize('0xB9EB427911BAb56E8B7683cC3d82821B44d2c7cc');
-        MicroRaiden.token.balanceOf.call(receiver, {from: receiver}, (error, result) => {
+        MicroRaiden.token.balanceOf.call(sender, {from: receiver}, (error, result) => {
             console.log(result.toNumber());
         });
     },
